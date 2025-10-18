7ca8974213bcb6e9beff4c155f6de50cfab74968
hoangquyctxh <hoangquy.ctxh@gmail.com>
2025-10-18 19:12:36 +0700

    Update locations.js

--- locations.js
+++ locations.js
@@ -18,29 +18,39 @@ const LOCATIONS_CONFIG = [
         name: 'Điện Biên Phủ', 
         lat: 21.032432212532083, 
         lng: 105.83939563968067,
-        capacity: 150 // <--- THÊM SỨC CHỨA Ở ĐÂY
+        capacity: 150,
+        address: '22 Điện Biên Phủ, Ba Đình, Hà Nội',
+        hotline: '0987.654.321',
+        operatingHours: '11:00 – 22:00'
     },
     { 
         id: 'THCSNCT', 
         name: 'Trung Học Cơ Sở Nguyễn Công Trứ', 
         lat: 21.04238224912817,
         lng: 105.84632614752188,
-        capacity: 150 // <--- THÊM SỨC CHỨA Ở ĐÂY
+        capacity: 150,
+        address: '6 P. Nguyễn Trường Tộ, Nguyễn Trung Trực, Ba Đình',
+        hotline: '0988.888.888',
+        operatingHours: '07:00 – 17:00'
     },
     { 
         id: 'VHVANXUAN', 
         name: 'Vườn Hoa Vạn Xuân', 
         lat: 21.040082771937307,
         lng: 105.84675825624123,
-        capacity: 300 // <--- THÊM SỨC CHỨA Ở ĐÂY
+        capacity: 300,
+        address: 'Gần 16 P. Phan Đình Phùng, Quán Thánh, Ba Đình',
+        hotline: '0977.777.777',
+        operatingHours: '08:00 – 23:00'
     },
     { 
         id: 'THPTPDP', 
         name: 'Trung Học Phổ Thông Phan Đình Phùng', 
         lat: 21.040684,
         lng: 105.842947,
-        capacity: 150 // <--- THÊM SỨC CHỨA Ở ĐÂY
+        capacity: 150,
+        address: '67 P. Cửa Bắc, Quán Thánh, Ba Đình',
+        hotline: '0966.666.666',
+        operatingHours: '07:00 – 18:00'
     },
 ];
-
-
